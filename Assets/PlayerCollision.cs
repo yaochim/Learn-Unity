ac8f174bbcb51ccabb51f2f0524906693e77f82d
Yao on Flex-Ryxen-MXnix <yaochim8@gmail.com>
2020-11-02 21:42:41 +0000

    Learning unity part1
    with Brackeys on youtube

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+    void OnCollisionEnter(Collision colInfo)
+    {
+        if (colInfo.collider.tag == "Cube") {
+            movement.enabled=false;
+        }
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float forwardForce = 500f;
+    public float sideForce = 500f;
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    // Use FixedUpdate to work better with physics
+    void FixedUpdate()
+    {
+        // Add a forward force
+        if ( Input.GetKey("w") )
+        {
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        }
+        if ( Input.GetKey("s") )
+        {
+            rb.AddForce(0, 0, -forwardForce * Time.deltaTime);
+        }
+        
+        if ( Input.GetKey("d") )
+        {
+            rb.AddForce(sideForce * Time.deltaTime, 0, 0);
+        }
+        if ( Input.GetKey("a") )
+        {
+            rb.AddForce(-sideForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
